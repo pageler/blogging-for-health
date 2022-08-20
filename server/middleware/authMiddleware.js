84bcea50d59128373b9d68ef3c38665fda5bf0b8
John Paul Pageler Jr <harakido1@yahoo.com>
2022-08-20 19:53:56 -0400

    connected server userRoutes to mongoDB

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function (req, res, next) {
+    const token = req.header("x-auth-token");
+
+    if (!token)
+        return res
+            .status(401)
+            .json([
+                { message: "No token, authorization denied", type: "error" },
+            ]);
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded.user;
+        next();
+    } catch (error) {
+        res.status(401).json([{ message: "Invalid token", type: "error" }]);
+    }
+};
